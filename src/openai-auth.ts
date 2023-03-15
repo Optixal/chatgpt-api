e93af974393ce1f4dcae2b02ca9e0bc2eae3c0c0
Optixal <optixal@live.com.sg>
2023-03-16 01:46:50 +0800

    fix: windows chrome directory change to Program Files (x86)

--- src/openai-auth.ts
+++ src/openai-auth.ts
@@ -475,7 +475,7 @@ export const defaultChromeExecutablePath = (): string => {
 
   switch (os.platform()) {
     case 'win32':
-      return 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+      return 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
 
     case 'darwin':
       return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
